4b7e8919afcc60c3e7a4e63f3c294ffe3e8c4107
yassa-alqess <iscoadms2@gmail.com>
2024-07-26 23:34:38 +0300

    fix: fixed task-submission router

--- src/modules/routes.ts
+++ src/modules/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import taskRouter from "./tasks/tasks.api";
+import taskSubmissionRouter from "./task-submission/task-submission.api";
 
 const restRouter = Router();
 
 restRouter.use("/tasks", taskRouter);
-
+restRouter.use("/task-submission", taskSubmissionRouter);
 export default restRouter;
