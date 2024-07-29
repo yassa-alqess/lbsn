5a1e9b6c76d77a366da26de0cff4c6ec58d6b591
yassa-alqess <iscoadms2@gmail.com>
2024-07-29 19:14:42 +0300

    enhance: add comments to unless middleware

--- src/shared/utils/unless.ts
+++ src/shared/utils/unless.ts
@@ -1,3 +1,7 @@
+/**
+ * The `unless middleware` function is used to conditionally apply another middleware function based on the request path.
+ */
+
 import { Request, Response, NextFunction } from 'express';
 
 export const unless = (middleware: (req: Request, res: Response, next: NextFunction) => void, paths: string[]) => {
