6c88a9cb33b99d2e38bd11bad7666172e6b1fdaa
yassa-alqess <iscoadms2@gmail.com>
2024-07-29 19:11:27 +0300

    fix: remove unnecessary comments

--- src/modules/tasks/tasks.api.ts
+++ src/modules/tasks/tasks.api.ts
@@ -4,8 +4,7 @@ import TaskService from "./tasks.service";
 const taskRouter = express.Router();
 const taskController = new TaskController(new TaskService());
 
-// taskRouter.post("/submitTaskResponse", upload("tasks")!.single("file"), taskController.submitTaskResponse);
 taskRouter.post("/get-tasks", taskController.getTasks);
 taskRouter.post("/add-task", taskController.addTask);
-// taskRouter.post("/resolveTask", taskController.resolveTask);
+
 export default taskRouter;
