d4846166fd8b814b32896a4bb8833b575152f0b5
yassa-alqess <iscoadms2@gmail.com>
2024-07-31 19:05:05 +0300

    feat: add profiles module

---
+++ src/modules/profiles/profiles.service.ts
@@ -0,0 +1,59 @@
+import { IProfileAddPayload, IProfileResponse, IProfilesGetResponse, IProfileUpdatePayload } from "./profiles.interface";
+import Profile from "../../shared/models/profile";
+import { ProfileAlreadyExistsError } from "../../shared/errors";
+
+export default class ProfileService {
+    public async addProfile(profilePayload: IProfileAddPayload): Promise<IProfileResponse> {
+        const profile = await Profile.findOne({ where: { name: profilePayload.name } });
+        if (profile) {
+            throw new ProfileAlreadyExistsError('Profile already exists');
+        }
+        const newProfile = await Profile.create({ ...profilePayload });
+        return {
+            profileId: newProfile.profileId,
+            name: newProfile.name,
+        };
+    }
+
+    public async updateProfile(profilePayload: IProfileUpdatePayload): Promise<IProfileResponse> {
+        const { profileId } = profilePayload;
+        const profile = await Profile.findByPk(profileId);
+        if (!profile) {
+            throw new Error('Profile not found');
+        }
+        await profile.update({ ...profilePayload });
+        return {
+            profileId: profile.profileId,
+            name: profile.name,
+        };
+    }
+    public async getProfile(profileId: string): Promise<IProfileResponse> {
+        const profile = await Profile.findByPk(profileId);
+        if (!profile) {
+            throw new Error('Profile not found');
+        }
+        return {
+            profileId: profile.profileId,
+            name: profile.name,
+        };
+    }
+
+    public async getProfiles(): Promise<IProfilesGetResponse> {
+        const profiles = await Profile.findAll();
+        return {
+            profiles:
+                profiles.map(profile => ({
+                    profileId: profile.profileId,
+                    name: profile.name,
+                }))
+        }
+    }
+
+    public async deleteProfile(profileId: string): Promise<void> {
+        const profile = await Profile.findByPk(profileId);
+        if (!profile) {
+            throw new Error('Profile not found');
+        }
+        await profile.destroy();
+    }
+}
