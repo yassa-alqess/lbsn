f8fcc0a9e80d5f37e22b6fbcc294c28085753758
yassa-alqess <iscoadms2@gmail.com>
2024-07-31 19:16:48 +0300

    fix: add missing controllers to startup procedure

--- src/startup.ts
+++ src/startup.ts
@@ -1,22 +1,37 @@
 // file dependinces
 import './config/env'
-import TicketController from "./modules/tickets/tickets.controller";
-import TaskController from "./modules/tasks/tasks.controller";
-import UserController from "./modules/users/users.controller";
-import TaskSubmissionController from "./modules/task-submission/task-submission.controller";
 import logger from "./config/logger";
 import { closeConnection } from "./config/database/connection";
 import App from './app';
 
+import TicketController from "./modules/tickets/tickets.controller";
+import TaskController from "./modules/tasks/tasks.controller";
+import UserController from "./modules/users/users.controller";
+import TaskSubmissionController from "./modules/task-submission/task-submission.controller";
+import UserProfilesController from './modules/user-profiles/user-profiles.controller';
+import ProfileController from './modules/profiles/profiles.controller';
+import GuestController from './modules/guests/guests.controller';
+import ServiceController from './modules/services/services.controller';
+import TimeSlotsController from './modules/time-slots/time-slots.controller';
+import AppointmentController from './modules/appointments/appointments.controller';
+
 // 3rd-party dependencies
 import { Server } from "http";
+
 
 const app = new App([
 
   new UserController(),
   new TicketController(),
   new TaskController(),
-  new TaskSubmissionController()
+  new TaskSubmissionController(),
+  new ProfileController(),
+  new GuestController(),
+  new ServiceController(),
+  new UserProfilesController(),
+  new ServiceController(),
+  new TimeSlotsController(),
+  new AppointmentController(),
 
 ]);
 // startup script
