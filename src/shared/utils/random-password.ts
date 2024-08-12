08ca6880a256cd11af64dde7561ae625e7b040f4
yassa-alqess <iscoadms2@gmail.com>
2024-08-12 19:13:22 +0300

    refactor: Update generateRandomPassword function to use named export

--- src/shared/utils/random-password.ts
+++ src/shared/utils/random-password.ts
@@ -1,5 +1,5 @@
 import crypto from 'crypto'
-export default function generateRandomPassword(length = 8) {
+export  function generateRandomPassword(length = 8) {
 
     return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
 }
