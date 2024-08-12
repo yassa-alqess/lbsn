35eb15c652fa67ee81ab9bee812bc3c81d189a21
yassa-alqess <iscoadms2@gmail.com>
2024-08-12 19:15:42 +0300

    refactor: Update MeetingService to use account credentials for access token grant type

--- src/modules/meetings/meeting.service.ts
+++ src/modules/meetings/meeting.service.ts
@@ -1,34 +1,56 @@
 import axios from 'axios'
 import { MEETING_API_URL, MEETING_AUTH_API_URL, MEETING_ACCOUNT_ID, MEETING_CLIENT_ID, MEETING_CLIENT_SECRET } from '../../shared/constants/index'
+import logger from '../../config/logger'
 
 export default class MeetingService {
-    private _getAccessToken = async () => {
-        const url = `${MEETING_AUTH_API_URL}/oauth/token?grant_type=client_credentials&account_id=${MEETING_ACCOUNT_ID}`
+
+    public _getAccessToken = async () => {
+        const url = `${MEETING_AUTH_API_URL}/oauth/token?grant_type=account_credentials&account_id=${MEETING_ACCOUNT_ID}`
         const headers = {
-            Authorization: `Basic ${Buffer.from(`${MEETING_CLIENT_ID}:${MEETING_CLIENT_SECRET}`).toString('base64')}`
+            Authorization: `Basic ${Buffer.from(`${MEETING_CLIENT_ID}:${MEETING_CLIENT_SECRET}`).toString('base64')}`,
         }
-        const response = await axios.post(url, {}, { headers })
+
+        const response = await axios.post(url, {}, { headers });
         return response.data.access_token
     }
 
-    public sheduleMeeting = async (topic: string, startTime: Date, password: string) => {
+    public scheduleMeeting = async (topic: string, startTime: Date, password: string) => {
 
         const url = `${MEETING_API_URL}`
-        const token = await this._getAccessToken();
+        let token;
+        try {
+
+            token = await this._getAccessToken();
+            //eslint-disable-next-line
+        } catch (err) {
+            logger.error(`Couldn't get access token ${err}`);
+            throw new Error("Couldn't get access token");
+        }
         const headers = {
-            Authorization: `Bearer ${token}`
+            Authorization: `Bearer ${token}`,
         }
         const payload = {
             topic,
             type: 2,
-            start_time: startTime,
+            start_time: "2024-08-01T14:25:00Z",
             timeZone: 'GMT+2',
             duration: 60,
             password,
             default_password: false,
             pre_schedule: false,
         }
-        const response = await axios.post(url, payload, { headers });
+
+        let response;
+        try {
+
+            response = await axios.post(url, payload, { headers });
+
+            //eslint-disable-next-line
+        } catch (err: any) {
+            logger.error(`Couldn't Create A Meeting ${err}`);
+            throw new Error("Couldn't Create A Meeting");
+        }
+
         return response.data;
     }
 }
