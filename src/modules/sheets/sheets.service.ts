905d59c9ae4349ecc1a5d85855f34491ae04a141
yassa-alqess <iscoadms2@gmail.com>
2024-08-20 15:32:02 +0300

    refactor: Update SheetsService to include methods for creating and sharing spreadsheets

--- src/modules/sheets/sheets.service.ts
+++ src/modules/sheets/sheets.service.ts
@@ -1,26 +1,50 @@
-import { google } from 'googleapis'
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'] // read and write access
+/* eslint-disable */
+
+import { google } from 'googleapis';
+
+const SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive',
+];
 
 export default class SheetsService {
-    //eslint-disable-next-line
-    public sheets: any;
+    private sheets: any;
+    private drive: any;
 
     constructor() {
-        this.sheets = google.sheets({ version: 'v4' })
+        this.sheets = google.sheets({ version: 'v4' });
+        this.drive = google.drive({ version: 'v3' });
     }
 
     private _getAuthToken = async () => {
         const auth = new google.auth.GoogleAuth({
-            scopes: SCOPES
+            keyFile: '.keys/ldns-sheets-d2c4a31efa5d.json', // Path to service account key
+            scopes: SCOPES,
         });
         try {
             const authToken = await auth.getClient();
             return authToken;
-        } //eslint-disable-next-line
+        }
         catch (error: any) {
             throw new Error(`Error getting google auth token: ${error.message}`);
         }
     }
+
+    public createSpreadSheet = async (title: string) => {
+        const auth = await this._getAuthToken();
+        const resource = {
+            properties: {
+                title,
+            },
+        };
+        const spreadsheet = await this.sheets.spreadsheets.create({
+            resource,
+            auth,
+        });
+        return spreadsheet.data;
+    }
+
     public getSpreadSheet = async (spreadsheetId: string) => {
         const auth = await this._getAuthToken();
         try {
@@ -29,7 +53,7 @@ export default class SheetsService {
                 auth,
             });
             return res;
-        } //eslint-disable-next-line
+        }
         catch (error: any) {
             throw new Error(`Error getting spreadsheet: ${error.message}`);
         }
@@ -44,9 +68,45 @@ export default class SheetsService {
                 range: sheetName
             });
             return res;
-        } //eslint-disable-next-line
+        }
         catch (error: any) {
             throw new Error(`Error getting spreadsheet values: ${error.message}`);
+        }
+    }
+
+    public shareSheetWithAnyone = async (spreadsheetId: string) => {
+        const auth = await this._getAuthToken();
+        try {
+            await this.drive.permissions.create({
+                fileId: spreadsheetId,
+                auth,
+                requestBody: {
+                    role: 'writer',
+                    type: 'anyone',
+                },
+            });
+        
+        } catch (error: any) {
+            throw new Error(`Error sharing spreadsheet with anyone: ${error.message}`);
+        }
+    }
+
+    public shareSheetWithEmail = async (spreadsheetId: string, email: string) => {
+        const auth = await this._getAuthToken();
+        try {
+            await this.drive.permissions.create({
+                fileId: spreadsheetId,
+                auth,
+                requestBody: {
+                    role: 'writer',
+                    type: 'user',
+                    emailAddress: email,
+                },
+                // transferOwnership: true,
+            });
+
+        } catch (error: any) {
+            throw new Error(`Error sharing spreadsheet with email: ${error.message}`);
         }
     }
 }
