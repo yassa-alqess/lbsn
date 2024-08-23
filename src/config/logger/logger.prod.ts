fd305b37bcf25962b7b45cfc874ede53a4366640
yassa-alqess <iscoadms2@gmail.com>
2024-08-23 15:18:19 +0300

    refactor: Add console transport to prod logger

--- src/config/logger/logger.prod.ts
+++ src/config/logger/logger.prod.ts
@@ -1,14 +1,17 @@
 import { createLogger, transports, format } from 'winston';
-const { combine, timestamp, printf } = format;
+const { combine, timestamp, printf, json, colorize } = format;
+console.log(process.env.LOG_LEVEL);
 export const prodLogger = createLogger({
   transports: [
     new transports.File({
       filename: '.logs/prod/prod.log',
     }),
+    new transports.Console({ format: format.combine(colorize(), format.simple()) }),
   ],
   level: process.env.LOG_LEVEL || 'info', // info and above
   format: combine(
-    timestamp(), // server time
+    timestamp({ format: 'HH:mm:ss' }),
+    json(),
     printf(({ timestamp, level, message, service = '' }) => {
       return `[${timestamp}] ${service} [${level}]: ${message}`;
     }),
