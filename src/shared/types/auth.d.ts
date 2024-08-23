03912abd554af57c69c68c9dbc83a08ff3f645a2
yassa-alqess <iscoadms2@gmail.com>
2024-08-23 15:15:59 +0300

    fix: fix express.request interface conflict

--- src/shared/types/auth.d.ts
+++
@@ -1,9 +0,0 @@
-import { IAuthPayload } from '../../modules/auth/auth.interface'; // Adjust the import path as necessary
-
-declare global {
-    namespace Express {
-        interface Request {
-            user?: IAuthPayload;
-        }
-    }
-}
