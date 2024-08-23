dfd684a44729d802369d360a76a68875e07ed4f0
yassa-alqess <iscoadms2@gmail.com>
2024-08-23 15:16:57 +0300

    refactor: Update logger configuration to use ENV constant

--- src/config/logger/index.ts
+++ src/config/logger/index.ts
@@ -2,5 +2,4 @@ import { devLogger } from './logger.dev';
 import { prodLogger } from './logger.prod';
 import { testLogger } from './logger.test-d';
 import { ENV } from '../../shared/constants';
-console.log("ENV", ENV);
-export default ENV === 'development' ? devLogger : ENV === 'testing' ? testLogger : prodLogger;
+export default logger = ENV === 'development' ? devLogger : ENV === 'testing' ? testLogger : prodLogger;
