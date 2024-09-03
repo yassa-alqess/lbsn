626647137cae837481d53ac72be57e27599efd5a
yassa-alqess <iscoadms2@gmail.com>
2024-09-03 16:36:34 +0300

    chore: add https support

--- src/config/ws/index.ts
+++ src/config/ws/index.ts
@@ -1,7 +1,12 @@
 import { ExtendedWebSocketServer } from '../../shared/interfaces';
 import { Server } from 'http';
-
+import { Server as SecureServer } from 'https';
 export const initWebSocket = (server: Server) => {
+    const WSS = new ExtendedWebSocketServer({ server });
+    return WSS;
+}
+
+export const initSecureWebSocket = (server: SecureServer) => {
     const WSS = new ExtendedWebSocketServer({ server });
     return WSS;
 }
