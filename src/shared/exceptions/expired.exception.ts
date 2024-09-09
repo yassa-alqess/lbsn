edb417577cca3273a7bf81c105802962dbd0dd33
yassa-alqess <iscoadms2@gmail.com>
2024-09-09 21:54:58 +0300

    feat: make expire exception generic

---
+++ src/shared/exceptions/expired.exception.ts
@@ -0,0 +1,8 @@
+import HttpException from './http.exception';
+import { StatusCodes } from 'http-status-codes';
+
+export class ExpiredException extends HttpException {
+    constructor(type: string) {
+        super(StatusCodes.BAD_REQUEST, `${type} expired or not valid`);
+    }
+}
