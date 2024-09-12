373ceffcd9388b0d551df712818f3c39ff9751e7
yassa-alqess <iscoadms2@gmail.com>
2024-09-12 17:31:00 +0300

    chore: add token/expiry constants

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -10,7 +10,8 @@ export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || ""
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || ""
 export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY || "15m"
 export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY || "7d"
-export const OTP_EXPIRY = process.env.OTP_EXPIRY || "5m"
+export const OTT_SECRET = process.env.OTT_SECRET || ""
+export const OTT_EXPIRY = process.env.OTT_EXPIRY || "3m"
 
 
 // mail constants
