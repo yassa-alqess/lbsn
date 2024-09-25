c4f8f9eaa1f3d72fa2a7de995b07df8f416534eb
yassa-alqess <iscoadms2@gmail.com>
2024-09-25 16:28:51 +0300

    feat: add sale module

---
+++ src/modules/sales/sales.controller.ts
@@ -0,0 +1,92 @@
+import { INVALID_UUID, SALES_PATH } from "../../shared/constants";
+import { Controller } from "../../shared/interfaces";
+import logger from "../../config/logger";
+import { accessTokenGuard, requireAnyOfThoseRoles, validate } from "../../shared/middlewares";
+import { InternalServerException, InvalidEnumValueException, InvalidIdException, ParamRequiredException } from "../../shared/exceptions";
+import { RoleEnum, SalesStageEnum } from "../../shared/enums";
+import { ISalesGetPayload, ISaleUpdatePayload } from "./sales.interface";
+import SalesService from "./sales.service";
+import { UpdateSaleSchema } from "./sales.dto";
+
+// 3rd party dependencies
+import express, { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from "http-status-codes";
+
+export default class SalesController implements Controller {
+    path = SALES_PATH;
+    router = express.Router();
+    private _salesService = new SalesService();
+
+    constructor() {
+        this._initializeRoutes();
+    }
+
+    private _initializeRoutes() {
+        this.router.all(`${this.path}*`, accessTokenGuard);
+        this.router.get(this.path, this.getSales); //filter by profileId & status [there is pagination]
+        this.router.get(`${this.path}/all`, requireAnyOfThoseRoles([RoleEnum.ADMIN, RoleEnum.SUPER_ADMIN]), this.getAllSalesGrouped); //get all sales grouped by profileId
+        this.router.patch(`${this.path}/:saleId`, validate(UpdateSaleSchema), this.updateSale); //update sale status
+    }
+    public getSales = async (req: Request, res: Response, next: NextFunction) => {
+        const { profileId } = req.query;
+        if (!profileId) {
+            return next(new ParamRequiredException('Sale-made', 'profileId'));
+        }
+        const { stage, limit, offset } = req.query;
+        if (stage && !Object.values(SalesStageEnum).includes(stage as SalesStageEnum)) {
+            return next(new InvalidEnumValueException('SaleStage'));
+        }
+
+        try {
+            const payload: ISalesGetPayload = {
+                profileId: profileId as string,
+                stage: stage as SalesStageEnum,
+                limit: limit ? parseInt(limit as string) : undefined,
+                offset: offset ? parseInt(offset as string) : undefined
+            };
+            const sales = await this._salesService.getSales(payload);
+            res.status(StatusCodes.OK).json(sales).end();
+
+            //eslint-disable-next-line
+        } catch (error: any) {
+            logger.error('error at getSales action', error);
+            if (error?.original?.code == INVALID_UUID) { //invalid input syntax for type uuid
+                return next(new InvalidIdException('profileId'));
+            }
+            next(new InternalServerException(error.message));
+        }
+    };
+
+    public updateSale = async (req: Request, res: Response, next: NextFunction) => {
+        const { saleId } = req.params;
+        if (!saleId) return next(new ParamRequiredException('Sale-made', 'saleId'));
+        try {
+            const saleUpdatePayload: ISaleUpdatePayload = {
+                ...req.body, //status
+                saleId
+            }
+            await this._salesService.updateSale(saleUpdatePayload);
+            res.status(StatusCodes.OK).json({}).end();
+
+            //eslint-disable-next-line
+        } catch (error: any) {
+            logger.error(`error at UpdateSale action ${error}`);
+            if (error?.original?.code == INVALID_UUID) { //invalid input syntax for type uuid
+                return next(new InvalidIdException('saleId'));
+            }
+            next(new InternalServerException(error.message));
+        }
+    };
+
+    public getAllSalesGrouped = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const sales = await this._salesService.getAllSalesGrouped();
+            res.status(StatusCodes.OK).json(sales).end();
+
+            //eslint-disable-next-line
+        } catch (error: any) {
+            logger.error(`error at getAllSalesGrouped action ${error}`);
+            next(new InternalServerException(error.message));
+        }
+    };
+}
---
+++ src/modules/sales/sales.service.ts
@@ -0,0 +1,145 @@
+import logger from "../../config/logger";
+import { NotFoundException } from "../../shared/exceptions";
+import { LeadStatusEnum } from "../../shared/enums";
+import { GroupedSales, ISale, ISaleAddPayload, ISalesGetPayload, ISalesGetResponse, ISaleUpdatePayload } from "./sales.interface";
+import Sale from "../../shared/models/sale";
+import Lead from "../../shared/models/lead";
+import ProfileService from "../profiles/profiles.service";
+
+// 3rd party dependencies
+import { fn, col } from "sequelize";
+
+export default class SalesService {
+    private _profileService: ProfileService;
+    constructor() {
+        this._profileService = new ProfileService();
+    }
+
+    public async getSales(payload: ISalesGetPayload): Promise<ISalesGetResponse> {
+        try {
+            const { profileId, stage, limit, offset } = payload;
+            // check if profile exists
+            const profile = await this._profileService.getProfile(profileId);
+            if (!profile) {
+                throw new NotFoundException('Profile', 'profileId', profileId);
+            }
+            // Fetch leads and total count
+            const { rows: sales, count: total } = await Sale.findAndCountAll({
+                where: {
+                    profileId,
+                    ...(stage && { stage })
+                },
+                ...(limit !== undefined && { limit }),  // Apply limit only if provided
+                ...(offset !== undefined && { offset }), // Apply offset only if provided
+                order: [['createdAt', 'DESC']]
+            });
+
+            return {
+                sales: sales.map(sale => sale.toJSON() as ISale),
+                total
+            }
+            //eslint-disable-next-line
+        } catch (error: any) {
+            logger.error('Error fetching sales:', error.message);
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+            throw new Error('Failed to fetch sales');
+        }
+    }
+
+    public async updateSale(payload: ISaleUpdatePayload): Promise<void> {
+
+        try {
+            const { saleId, status, otherType, stage, dealValue, dealCurrency, comment } = payload;
+
+            const sale = await Sale.findByPk(saleId);
+            if (!sale) {
+                throw new NotFoundException('sale-made', 'saleId', saleId);
+            }
+
+            if (status) {
+                if (status !== LeadStatusEnum.SALE_MADE) {
+                    Lead.create({
+                        leadId: sale.saleId,
+                        profileId: sale.profileId,
+                        status,
+                        otherType,
+                        record: sale.record
+                    });
+
+                    await sale.destroy();
+                    return;
+                }
+                else {
+                    throw new Error('Invalid status for sale update');
+                }
+            }
+            await sale.update({
+                ...(stage && { stage }),
+                ...(dealValue && { dealValue }),
+                ...(dealCurrency && { dealCurrency }),
+                ...(comment && { comment })
+            });
+
+            //eslint-disable-next-line
+        } catch (error: any) {
+            logger.error(`Error updating sale: ${error.message}`);
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+            throw new Error(`Failed to update sale: ${error.message}`);
+        }
+    }
+
+    public async getAllSalesGrouped(): Promise<GroupedSales[]> {
+        try {
+            // Step 1: Fetch the sales Count grouped by profileId
+            const salesCount = await Sale.findAll({
+                attributes: [
+                    'profileId', // Group by profileId
+                    [fn('COUNT', col('saleId')), 'salesCount'], // Count sales per profile
+                ],
+                group: ['profileId'], // Grouping by profileId
+                order: [['profileId', 'ASC']] // Optional: order by profileId
+            });
+
+            // Prepare the result array
+            const groupedSales: GroupedSales[] = [];
+
+            // Step 2: Fetch all sales details for each profileId
+            for (const sale of salesCount) {
+                const profileId = sale.profileId;
+                const sales = await Sale.findAll({
+                    where: { profileId },
+                    order: [['createdAt', 'ASC']] // Optional: order sales
+                });
+
+                groupedSales.push({
+                    profileId,
+                    salesCount: sale.get('salesCount') as number,
+                    sales: sales.map(sale => sale.toJSON() as ISale)
+                });
+            }
+
+            return groupedSales;
+        } catch (error) {
+            logger.error('Error fetching all sales:', error);
+            throw new Error('Failed to fetch all leads');
+        }
+    }
+
+    public async addSale(payload: ISaleAddPayload): Promise<ISale> {
+        try {
+            const sale = await Sale.create({ ...payload });
+            return {
+                ...sale.toJSON() as ISale
+            }
+
+            //eslint-disable-next-line
+        } catch (error: any) {
+            logger.error('Error adding sale:', error.message);
+            throw new Error('Failed to add sale');
+        }
+    }
+}
